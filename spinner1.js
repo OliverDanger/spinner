5578f5414f886e1a2656f18343b0449fa3558717
OliverDanger <od.oliverdixon@gmail.com>
2023-03-14 18:02:05 +0000

    made spinner micro-animation

---
+++ spinner1.js
@@ -0,0 +1,37 @@
+// process.stdout.write('hello from spinner1.js...  \rheyyy\n') <= hello world
+
+setTimeout(() => {
+  process.stdout.write('\r|  ')
+}, 60);
+
+setTimeout(() => {
+  process.stdout.write('\r/  ')
+}, 180);
+
+setTimeout(() => {
+  process.stdout.write('\r-  ')
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\  ')
+}, 420);
+
+setTimeout(() => {
+  process.stdout.write('\r|  ')
+}, 360);
+
+setTimeout(() => {
+  process.stdout.write('\r/  ')
+}, 180);
+
+setTimeout(() => {
+  process.stdout.write('\r-  ')
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\  ')
+}, 420);
+
+setTimeout(() => {
+  process.stdout.write('\r|  ')
+}, 360);
